7ff206db4c4dda68dc62d1b6c3bcce7828f2a51d
YeaJeong <bananana0118@gmail.com>
2022-10-30 12:06:26 +0900

    Create 프로그래머스_체육복.py

---
+++ .ipynb_checkpoints/프로그래머스_체육복.py
@@ -0,0 +1,37 @@
+def solution(n, lost, reserve):
+    dic1 = {}
+    
+    for x in range(1,n+1 ):
+        dic1[x] = dic1.get(x,1)
+
+    for x in lost : 
+        dic1[x] = dic1.get(x , 0) -2
+
+
+    for x in reserve:
+        dic1[x] = dic1.get(x,0) +1
+    
+    for x in range (1,n+1) :
+        if (dic1[x] <0 ):
+            if (x > 1 and x < n):
+                if (dic1[x-1] > 1 ):
+                    dic1[x] = 1
+                    dic1[x-1] -= 1
+                elif (dic1[x+1] > 1):
+                    dic1[x] =  1
+                    dic1[x+1] -= 1
+            elif (x == 1):    
+                if (dic1[x+1] > 1):
+                    dic1[x] =  1
+                    dic1[x+1] -= 1
+            elif (x==n) :
+                if(dic1[x-1] > 1):
+                    dic1[x] =  1
+                    dic1[x-1] -= 1
+
+    count = 0
+    for k, v in dic1.items():
+        if dic1[k] >= 0:
+            count +=1
+            
+    return count
